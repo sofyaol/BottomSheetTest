17e3e6b859621949a311d727fcc9768fe5efd0f7
oles <oles@omegafuture.ru>
2022-09-08 17:44:11 +0300

    Bottomsheet for IOS and Android

---
+++ BottomSheetTest/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace BottomSheetTest;
+
+public partial class MainPage : ContentPage
+{
+	
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void ShowBottomSheet(object sender, EventArgs e)
+    {
+        this.ShowBottomSheet(GetBottomSheetView(), true);
+    }
+
+
+    private View GetBottomSheetView()
+    {
+        var view = (View)BottomSheetTemplate.CreateContent();
+        view.BindingContext = BindingContext;
+        return view;
+    }
+
+}
+
